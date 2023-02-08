541fc8ba1432e7f4605af06c855c2a969cc71277
arvi-arkadia <120647001+arvi-arkadia@users.noreply.github.com>
2023-02-08 08:19:55 +0700

    Details Screens - app bar - leading icon - action icon

---
+++ lib/screens/details/components/details_app_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ui_orderfood_app/constants.dart';
+
+AppBar detailsAppBar() {
+  return AppBar(
+    leading: IconButton(
+      onPressed: () {},
+      icon: Icon(
+        Icons.arrow_back,
+        color: Colors.white,
+      ),
+    ),
+    elevation: 0,
+    backgroundColor: kPrimaryColor,
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset("assets/icons/share.svg"),
+      ),
+      IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset("assets/icons/more.svg"),
+      ),
+    ],
+  );
+}
